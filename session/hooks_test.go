90811ede92f48fe5cc0d912806d9aad91f134a24
sysatom <sysatom@gmail.com>
2020-10-12 15:24:48 +0800

    feat: hook

---
+++ session/hooks_test.go
@@ -0,0 +1,37 @@
+package session
+
+import (
+	"github.com/sysatom/orm/log"
+	"testing"
+)
+
+type Account struct {
+	ID       int `orm:"PRIMARY KEY"`
+	Password string
+}
+
+func (a *Account) BeforeInsert(s *Session) error {
+	log.Info("before insert", a)
+	a.ID += 1000
+	return nil
+}
+
+func (a *Account) AfterQuery(s *Session) error {
+	log.Info("after query", a)
+	a.Password = "******"
+	return nil
+}
+
+func TestSession_CallMethod(t *testing.T) {
+	s := NewSession().Model(&Account{})
+	_ = s.DropTable()
+	_ = s.CreateTable()
+	_, _ = s.Insert(&Account{1, "123456"}, &Account{2, "qwerty"})
+
+	u := &Account{}
+
+	err := s.First(u)
+	if err != nil || u.ID != 1001 || u.Password != "******" {
+		t.Fatal("Failed to call hooks after query, got", u)
+	}
+}
