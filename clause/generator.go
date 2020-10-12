40dc0c4b26cdb867715bfe228d502e33df5bdf71
sysatom <sysatom@gmail.com>
2020-10-12 15:02:04 +0800

    feat: update, delete, count

--- clause/generator.go
+++ clause/generator.go
@@ -17,6 +17,9 @@ func init() {
 	generators[LIMIT] = _limit
 	generators[WHERE] = _where
 	generators[ORDERBY] = _orderBy
+	generators[UPDATE] = _update
+	generators[DELETE] = _delete
+	generators[COUNT] = _count
 }
 
 func genBindVars(num int) string {
@@ -75,4 +78,27 @@ func _where(values ...interface{}) (string, []interface{}) {
 func _orderBy(values ...interface{}) (string, []interface{}) {
 	// ORDER BY $field
 	return fmt.Sprintf("ORDER BY %s", values[0]), []interface{}{}
+}
+
+func _update(values ...interface{}) (string, []interface{}) {
+	// UPDATE $tableName SET $values
+	tableName := values[0]
+	m := values[1].(map[string]interface{})
+	var keys []string
+	var vars []interface{}
+	for k, v := range m {
+		keys = append(keys, k+" = ?")
+		vars = append(vars, v)
+	}
+	return fmt.Sprintf("UPDATE %s SET %s", tableName, strings.Join(keys, ", ")), vars
+}
+
+func _delete(values ...interface{}) (string, []interface{}) {
+	// DELETE FROM $tableName
+	return fmt.Sprintf("DELETE FROM %s", values[0]), []interface{}{}
+}
+
+func _count(values ...interface{}) (string, []interface{}) {
+	// SELECT COUNT(*) FROM $tableName
+	return _select(values[0], []string{"COUNT(*)"})
 }
--- session/record_test.go
+++ session/record_test.go
@@ -5,7 +5,7 @@ import "testing"
 var (
 	user1 = &User{"Tom", 18}
 	user2 = &User{"Sam", 21}
-	user3 = &User{"Jack", 32}
+	user3 = &User{"Jack", 25}
 )
 
 func testRecordInit(t *testing.T) *Session {
@@ -33,5 +33,35 @@ func TestSession_Find(t *testing.T) {
 	var users []User
 	if err := s.Find(&users); err != nil || len(users) != 2 {
 		t.Fatal("failed to query all")
+	}
+}
+
+func TestSession_Limit(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	err := s.Limit(1).Find(&users)
+	if err != nil || len(users) != 1 {
+		t.Fatal("failed to query with limit condition")
+	}
+}
+
+func TestSession_Update(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Update("Age", 30)
+	u := &User{}
+	_ = s.OrderBy("Age DESC").First(u)
+
+	if affected != 1 || u.Age != 30 {
+		t.Fatal("failed to update")
+	}
+}
+
+func TestSession_DeleteAndCount(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Delete()
+	count, _ := s.Count()
+
+	if affected != 1 || count != 1 {
+		t.Fatal("failed to delete or count")
 	}
 }
